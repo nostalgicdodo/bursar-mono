3a6651fed4b5bf85430f3e5a16e46b332e4fed3f
adi <8440761+nostalgicdodo@users.noreply.github.com>
2024-06-10 11:08:43 +0530

    stash

--- ui/utils/logging.js
+++ ui/utils/logging.js
@@ -7,11 +7,12 @@ export function log ( name, context, scope ) {
 	headers.append( "Content-Type", "application/json" )
 
 	name = name.toLowerCase()
-	return window.fetch( `/api/v1/${ scope }/register_event`, {
-		method: "POST",
-		headers,
-		credentials: "include",
-		body: JSON.stringify( { type: name, context } ),
-		redirect: "follow"
-	} )
+	return Promise.resolve()
+	// return window.fetch( `/api/v1/${ scope }/register_event`, {
+	// 	method: "POST",
+	// 	headers,
+	// 	credentials: "include",
+	// 	body: JSON.stringify( { type: name, context } ),
+	// 	redirect: "follow"
+	// } )
 }
